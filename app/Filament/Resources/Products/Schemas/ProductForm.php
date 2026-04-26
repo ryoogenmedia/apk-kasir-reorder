5156058adce2d4907a9525a6a2229f79f98e4881
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2026-04-26 16:30:21 +0800

    feat: create Filament product resource with dedicated form and table configurations

---
+++ app/Filament/Resources/Products/Schemas/ProductForm.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Filament\Resources\Products\Schemas;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
+use Filament\Schemas\Schema;
+
+class ProductForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('category_id')
+                    ->required()
+                    ->numeric(),
+                TextInput::make('name')
+                    ->required(),
+                Textarea::make('description')
+                    ->columnSpanFull(),
+                TextInput::make('price')
+                    ->required()
+                    ->numeric()
+                    ->prefix('$'),
+                TextInput::make('stock')
+                    ->required()
+                    ->numeric()
+                    ->default(0),
+                TextInput::make('low_stock_threshold')
+                    ->required()
+                    ->numeric()
+                    ->default(5),
+            ]);
+    }
+}
