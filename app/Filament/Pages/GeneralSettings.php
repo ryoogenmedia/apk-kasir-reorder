5d8da8d3be95103b7d5d6944f38b9ffac17d54a6
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2026-04-26 16:54:29 +0800

    feat: create GeneralSettings Filament page for managing site branding and name

--- app/Filament/Pages/GeneralSettings.php
+++ app/Filament/Pages/GeneralSettings.php
@@ -14,7 +14,7 @@ class GeneralSettings extends Page
 {
     protected static string | \BackedEnum | null $navigationIcon = 'heroicon-o-cog-6-tooth';
 
-    protected static string $view = 'filament.pages.general-settings';
+    protected string $view = 'filament.pages.general-settings';
 
     protected static string | \UnitEnum | null $navigationGroup = 'Settings';
 
