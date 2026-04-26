14bf832c55c31e1a9b4b9fa7e0a9bc023b2fdddf
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2026-04-26 17:32:56 +0800

    feat: add nullable image column to products table

--- database/migrations/2026_04_26_093150_add_image_to_products_table.php
+++ database/migrations/2026_04_26_093150_add_image_to_products_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->string('image')->nullable()->after('description');
         });
     }
 
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('products', function (Blueprint $table) {
-            //
+            $table->dropColumn('image');
         });
     }
 };
