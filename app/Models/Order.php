71b37f5a574991db19853ffe2d688fd0d69e5e8b
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2026-04-26 16:15:31 +0800

    feat: create Order model with relationships to User and OrderItem

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-    //
+    protected $guarded = [];
+
+    protected $casts = [
+        'order_date' => 'date',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
