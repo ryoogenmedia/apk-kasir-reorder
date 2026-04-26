3f7fcbee4f7347f9f8bfb357d1b9971a3525c7f3
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2026-04-26 16:39:30 +0800

    feat: add SalesReport filament page restricted to super_admin role

--- app/Filament/Pages/SalesReport.php
+++ app/Filament/Pages/SalesReport.php
@@ -6,5 +6,14 @@ use Filament\Pages\Page;
 
 class SalesReport extends Page
 {
+    protected static ?string $navigationIcon = 'heroicon-o-document-chart-bar';
+
     protected string $view = 'filament.pages.sales-report';
+
+    protected static $navigationGroup = 'Laporan';
+
+    public static function canAccess(): bool
+    {
+        return auth()->user()->hasRole('super_admin');
+    }
 }
