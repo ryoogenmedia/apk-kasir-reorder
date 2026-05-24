835bb870a36fb947a698c3c0edbf9d647b4642b3
M. Bintang Ramli. <90763575+bintang-r@users.noreply.github.com>
2026-05-24 17:16:25 +0800

    feat: integrate SimpleXLSX library and add test file for spreadsheet parsing

---
+++ test.php
@@ -0,0 +1,10 @@
+<?php
+require 'app/Services/SimpleXLSX.php';
+$xlsx = \Shuchkin\SimpleXLSX::parse('public/template/example/data_produk_dan_category.xlsx');
+if ($xlsx) {
+    foreach (array_slice($xlsx->rows(1), 0, 5) as $r) {
+        print_r($r);
+    }
+} else {
+    echo \Shuchkin\SimpleXLSX::parseError();
+}
